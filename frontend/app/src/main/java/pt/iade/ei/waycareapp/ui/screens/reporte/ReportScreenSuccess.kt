9f363240fc1a61fd56571af52f351fde27ff9b75
macazava <macazava@gmail.com>
2025-10-23 18:38:35 +0100

    atualizei strings.xml com a info da tela de sucesso de reporte

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/reporte/ReportScreenSuccess.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/reporte/ReportScreenSuccess.kt
@@ -4,9 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
