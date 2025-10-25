ffd7fb291a044042f6b6c4db9a850df8f8011c2a
macazava <macazava@gmail.com>
2025-10-25 18:27:14 +0100

    criação de um preview simulado no MapaScreen para testar layout

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/mapa/MapaScreen.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/screens/mapa/MapaScreen.kt
@@ -7,12 +7,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 
 @SuppressLint("MissingPermission")
 @Composable
@@ -47,4 +55,31 @@ fun MapaScreen() {
         }
     )
 }
+
+
+/*criar um Preview simulado
+criar uma versão visual falsa da MapaScreen,
+só para mostrar no Preview, sem o mapa real.
+Assim se mantem a consistência visual e da para testar layout.*/
+@Composable
+fun MapaScreenPreviewFake() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Mapa aqui (simulação)",
+            color = Color.Gray
+        )
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MapaScreenPreview() {
+    MapaScreenPreviewFake()
+}
+
+
 
