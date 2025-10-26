479e47a2e0640028039f597af15b8912be3edf71
macazava <macazava@gmail.com>
2025-10-26 20:09:15 +0000

    atualizei strings.xml com a informação do cardObstaculo

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/component/cardObtaculo.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/ui/component/cardObtaculo.kt
@@ -1,13 +1,10 @@
 package pt.iade.ei.waycareapp.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
