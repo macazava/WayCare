a2760dda3f8814d9ec3e2ae8334ac9c26be756a4
macazava <macazava@gmail.com>
2025-10-26 16:33:24 +0000

    atualizei o ficheiro strings.xml com as informações da tela de Mapa

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/data/models/Reporte.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/data/models/Reporte.kt
@@ -1,4 +1,10 @@
 package pt.iade.ei.waycareapp.data.models
 
-class Reporte {
-}
+data class Reporte(
+    val id: Int,
+    val tipo: String, // Ex: "Incidente", "Serviço", "Rampa Inexistente"
+    val prioridade: String, // Ex: "Alta", "Média", "Baixa"
+    val latitude: Double,
+    val longitude: Double,
+    val descricao: String
+)
