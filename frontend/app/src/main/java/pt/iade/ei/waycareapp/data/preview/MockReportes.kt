6b953b5be5d4686d24c45cacc92de1f096ec6ef6
macazava <macazava@gmail.com>
2025-10-26 17:52:59 +0000

    criei o package components e preview com os files 'cardObstaculo' e 'MockReportes'

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/data/preview/MockReportes.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/data/preview/MockReportes.kt
@@ -1,2 +1,39 @@
 package pt.iade.ei.waycareapp.data.preview
 
+import pt.iade.ei.waycareapp.data.model.Categoria
+import pt.iade.ei.waycareapp.data.model.Localizacao
+import pt.iade.ei.waycareapp.data.model.Obstaculo
+import pt.iade.ei.waycareapp.data.model.Reporte
+import pt.iade.ei.waycareapp.data.model.Utilizador
+import java.time.LocalDateTime
+
+val mockReportes = listOf(
+    Reporte(
+        id = 1,
+        utilizador = Utilizador(1, "Maria", "maria@email.com", "1234", "912345678"),
+        obstaculo = Obstaculo(
+            id = 1,
+            categoria = Categoria(1, "Rampa Inexistente", "Não há rampa de acesso"),
+            descricao = "Sem rampa na entrada",
+            grauPerigo = "Médio"
+        ),
+        localizacao = Localizacao(1, 38.717, -9.139, "Rua A"),
+        data = LocalDateTime.now(),
+        estado = "Pendente",
+        comentario = "Muito difícil para cadeiras de rodas"
+    ),
+    Reporte(
+        id = 2,
+        utilizador = Utilizador(2, "João", "joao@email.com", "1234", "912123456"),
+        obstaculo = Obstaculo(
+            id = 2,
+            categoria = Categoria(1, "Rampa Inexistente", "rampa inexistente"),
+            descricao = "Sem rampa na entrada",
+            grauPerigo = "Médio"
+        ),
+        localizacao = Localizacao(1, 38.717, -9.139, "Rua A"),
+        data = LocalDateTime.now(),
+        estado = "Pendente",
+        comentario = "Muito difícil para cadeiras de rodas"
+    )
+)
