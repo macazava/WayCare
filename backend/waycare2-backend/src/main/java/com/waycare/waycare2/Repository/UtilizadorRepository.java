4aaea4aaf605a01a4f5ac3d9c9c71a2139f69cfa
macazava <macazava@gmail.com>
2025-11-08 20:31:30 +0000

    Delete backend directory

--- backend/waycare2-backend/src/main/java/com/waycare/waycare2/Repository/UtilizadorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.waycare.waycare2.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.waycare.waycare2.Model.Utilizador;
-
-public interface UtilizadorRepository extends JpaRepository<Utilizador, Long> {
-}
