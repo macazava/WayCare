b7076f2c35bdf7801e9163fe1e6b5f700ad461f8
macazava <macazava@gmail.com>
2025-11-18 18:01:41 +0000

    acrescentei a implementation do exif

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
 
 
     testImplementation(libs.junit)
