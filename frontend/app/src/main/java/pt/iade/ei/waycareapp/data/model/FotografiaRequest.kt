ac81064edb109528d20e9902d4a9fe7f74793c49
macazava <macazava@gmail.com>
2025-12-10 18:02:37 +0000

    teste de commit

--- frontend/app/src/main/java/pt/iade/ei/waycareapp/data/model/FotografiaRequest.kt
+++ frontend/app/src/main/java/pt/iade/ei/waycareapp/data/model/FotografiaRequest.kt
@@ -1,26 +1,15 @@
 package pt.iade.ei.waycareapp.data.model
 
-import com.google.gson.annotations.SerializedName
-
 data class FotografiaRequest(
-    @SerializedName("nome")
     val nome: String,
-
-    @SerializedName("caminho")
     val caminho: String? = null,
-
-    @SerializedName("mime")
     val mime: String,
-
-    @SerializedName("tamanho")
     val tamanho: Long,
-
-    @SerializedName("descricao")
     val descricao: String? = null,
-
-    @SerializedName("url")
-    val url: String, // aqui vai o link final do Cloudinary
-
-    @SerializedName("reporte")
-    val reporteId: Long
+    val url: String,
+    val reporteId: Long,
+    val anomaliaId: Long? = null,
+    val utilizadorId: Long? = null
 )
+
+//.
